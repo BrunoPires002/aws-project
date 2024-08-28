8ea07923de8f13807297f7b52f2b916893cbbb58
brunopires002 <brunoteixeirapires002@gmail.com>
2024-08-27 22:33:12 -0300

    Criando a tabela do DynamoDB.

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -5,12 +5,26 @@ import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 
 import { Construct } from "constructs"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly productsDdb: dynamodb.Table //criando uma classe para a tabela dynamoDB
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props)
+
+        this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+            tableName: "products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY, //faz com que os dados do banco sejam destruidos caso excluirmos as stacks
+            partitionKey: {
+                name: "id",
+                type: dynamodb.AttributeType.STRING, 
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
 
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
             "ProductsFetchFunction", {
